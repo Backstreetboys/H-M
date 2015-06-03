06de1b06d2e60e34ffa68fc45aefd743b18cdd73
Martiiiin756 <martiiiin756@gmail.com>
2015-06-03 12:31:48 +0200

    index och sass fix
    Fixade så att divarna funkar och tog bort lite i sass

--- index.php
+++ index.php
@@ -1,20 +1,23 @@
-<div>
-<?php
-if(have_posts()){
-	while(have_posts()){
-		the_post();
-		?>
-		<div>
-			<?php
-			the_title();
-			if(has_post_thumbnail()){
-				the_post_thumbnail();
-			}
-			?>
-		<div/>
+<div id="main-wrapper">
+	
 		<?php
-
-	}
-}
-?>
+		if(have_posts()){
+		
+			
+			while(have_posts()){ ?>
+				<div id="product-wrapper">
+					<?php
+					the_post();
+						the_title();
+						if(has_post_thumbnail()){
+							the_post_thumbnail();
+						}
+						?>
+				</div>
+				<?php
+			}
+			
+		}
+	?>
+	
 </div>
