07eaa23d073404245d9dbb3dfd75cc3a53a01cf9
Martiiiin756 <martiiiin756@gmail.com>
2015-06-03 12:16:16 +0200

    lade till divar i index

--- index.php
+++ index.php
@@ -1,13 +1,20 @@
+<div>
 <?php
 if(have_posts()){
 	while(have_posts()){
 		the_post();
-
-		the_title();
-		if(has_post_thumbnail()){
-			the_post_thumbnail();
-		}
+		?>
+		<div>
+			<?php
+			the_title();
+			if(has_post_thumbnail()){
+				the_post_thumbnail();
+			}
+			?>
+		<div/>
+		<?php
 
 	}
 }
 ?>
+</div>
