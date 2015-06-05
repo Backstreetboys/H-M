b84a6d8c2b91540a00d02870cc89b3eb50979d0b
Martiiiin756 <martiiiin756@gmail.com>
2015-06-05 14:07:01 +0200

    No conflicts

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
-var jQuery = $;
+var $ = jQuery;
 $(document).ready(function(){
 	$('#single-box').hide();
 	$('.product-wrapper').click(function(){
 		$('single-box').load('single.php');
+	});
+	$('.product-wrapper').click(function(){
+		$('#single-box').show();
+		$('#single-box').load('single.php');
 	});
 });
