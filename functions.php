2ac201c46dfcab3725e661743bf9d23c875e9ba5
Martiiiin756 <martiiiin756@gmail.com>
2015-06-05 13:40:03 +0200

    Lagt till jQeary

--- functions.php
+++ functions.php
@@ -1,3 +1,13 @@
 <?php
 /*Adds support for thumbnails*/
 add_theme_support('post-thumbnails');
+
+
+function load_javascript_files() {
+  wp_enqueue_script('jquery');
+  wp_register_script( 'main-js', get_template_directory_uri() . '/js/main.js', array('jquery'),'',true  );
+  wp_enqueue_script( 'main-js' );
+}
+
+add_action( 'wp_enqueue_scripts', 'load_javascript_files' );
+?>
--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
+var jQuery = $;
 $(document).ready(function(){
+	$('#single-box').hide();
 	$('.product-wrapper').click(function{
 		$('single-box').load('single.php');
 	});
