54c84ad518d74df195f21c127f6c015d5252bad2
Martiiiin756 <martiiiin756@gmail.com>
2015-08-26 15:23:12 +0200

    blääää

--- templates/page-template_checkout.php
+++ templates/page-template_checkout.php
@@ -10,29 +10,36 @@ get_header(); ?>
 
 <div id="checkout-list">
 
-	<div id="checkout-cat">
-		<h1 id="cat-1">Product</h1> <h1 id="cat-2">Size</h1> <h1 id="cat-3">Price</h1></h1>
-	</div>
-	<div id="item-list">
+
+	<table class="checkout-table">
+		<tr class="table-head">
+			<th>Products</th>
+			<th>Size</th>
+			<th>Price</th>
+		</tr>
+		
 		<?php
 		$sum = 0;
 		$item = json_decode($_SESSION['shoppingcart'], true);
 			foreach ($item['data'] as $result) { ?>
-
-				<p id="checkout-product-info"><?php echo $result['name']; ?> <span id="span1"><?php echo $result['size']; ?></span> <span id="span2"><?php echo $result['price']; ?>kr</span></p><br>
+				
+					<tr class="table-body">
+						<td><?php echo $result['name']; ?></td>
+						<td><?php echo $result['size']; ?></td>
+						<td><?php echo $result['price']; ?>kr</td>
+						<td id="table-button"><button>remove X</button></td>
+					</tr>
 				
 				
 			<?php
 			$val + $result['price'];
-			}
-			echo $sum;
-			?>
+			} ?>
+	</table>
+<!-- 	<?php echo $sum; ?> -->	
 
-	</div>
-	<div id="pay">
-		<a href="index.php?page_id=228">Betala</a>
-
-	</div>
+</div>
+<div id="pay">
+		<a href="<?= get_template_directory_uri() ?>/loading">Betala</a>
 
 </div>
 
