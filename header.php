0b3d33990d3ff6e7ae4357cc7fb7f84465a6fce6
Erik Elmehed <super_erik3@hotmail.com>
2015-08-27 13:55:14 +0200

    Lite saker
    -

--- header.php
+++ header.php
@@ -59,7 +59,8 @@ session_start();
 		<!-- This div contains all the addes products that will be shown when you push the shopping bag icon -->
 		<!-- links to the checkout.php when you whant to check out -->
 		<div id="item-holder">
-			<h1><a href="<?= get_template_directory_uri(); ?>/checkout">checkout</a></h1>
+			<i class="fa fa-arrow-right"></i>
+			<a href="<?= get_template_directory_uri(); ?>/checkout">Checkout<i class="fa fa-arrow-right"></i></a>
 		</div><!-- Ends the item holder div -->
 		
 
